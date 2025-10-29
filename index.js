d70240469f52f175b782a5a9a037cef49a8c9d3f
Eduardo Professor <eduardo.luciano.professor@gmail.com>
2025-10-29 13:06:36 +0000

    init first page

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const express = require("express")
-const app = express()
+import express from "express";
+const app = express();
 
-app.listen(3000, ()=>{
-    console.log("port 3000")
-})
+import { PrismaClient } from "./generated/prisma/client.js";
 
-app.get("/", (req, res) =>{
-    res.send("hello world")
-})
+const prisma = new PrismaClient();
+
+app.listen(3000, () => {
+  console.log("port 3000");
+});
+
+app.get("/", async (req, res) => {
+  const products = await prisma.product.findMany();
+  console.log(products);
+  res.json(products);
+});
+
+app.get("/create", async (req, res) => {
+  const user = await prisma.product.create({
+    data: {
+      name: "gato",
+      price: "15.00",
+      description: "gato daora",
+    },
+  });
+
+  res.redirect("https://3s5tmk-5500.csb.app/");
+});
